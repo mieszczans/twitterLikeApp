69265a258509623a071967c89738d7fd7a0de04a
sebastian mieszczanczyk <sebastianmieszczanczyk@gmail.com>
2019-06-25 19:09:52 +0200

    fix import for welcome module

--- src/app/content/welcome/welcome.module.ts
+++ src/app/content/welcome/welcome.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
+import { WelcomeComponent } from './welcome.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    WelcomeComponent,
     WelcomeRoutingModule
   ],
   declarations: []
