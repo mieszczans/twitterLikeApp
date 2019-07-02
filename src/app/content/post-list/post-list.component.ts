62fb9fc9cc44c0f527e33688d07f2a4a6de62324
sebastian mieszczanczyk <sebastianmieszczanczyk@gmail.com>
2019-07-02 13:30:38 +0200

    fix animation

--- src/app/content/post-list/post-list.component.ts
+++ src/app/content/post-list/post-list.component.ts
@@ -10,9 +10,9 @@ import { map } from 'rxjs/operators';
   animations: [
     trigger('getPosts', [
       transition('* => *', [
-        query(':enter', style({transform: 'translateX(-100vw)', opacity: 0}), { optional: true }),
-        query(':leave', stagger('1000ms', [
-          animate('300ms', style({transform: 'translateX(0)', opacity: 100})),
+        query('.single-post', style({transform: 'translateX(-100vw)', opacity: 0}), { optional: true }),
+        query(':enter', stagger('1000ms', [
+          animate('500ms', style({transform: 'translateX(0)', opacity: 100})),
         ]), { optional: true }),
       ])
     ])
@@ -32,7 +32,8 @@ export class PostListComponent implements OnInit {
         const reversed = posts.reverse();
         this.postService.addPostsToSubject(reversed);
       },
-      () => {
+      (err) => {
+        console.log(err);
       }
     );
   }
